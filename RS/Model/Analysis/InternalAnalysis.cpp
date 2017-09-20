1cbd16bbd78c098d66f2c8f2511975691d3e3b9c
Sérgio Vargas Jr <sergio_vargas@outlook.com>
2017-09-20 17:40:59 -0300

    Fixed text maps showing wrong node IDs

--- RS/Model/Analysis/InternalAnalysis.cpp
+++ RS/Model/Analysis/InternalAnalysis.cpp
@@ -1,4 +1,5 @@
 #include "Model/Analysis/InternalAnalysis.h"
+#include "Model/System/Defines.h"
 
 #include <cstdio>
 #include <math.h>
@@ -193,7 +194,7 @@ TrafficAnalysis::StatusAnalysis InternalAnalysis::analyzeAllLinks() {
             float avcpf = 0;
             float abw = 0;
             float thr = 0;
-            if(internalPackets[dim][link].size() > 0){ //TODO: verify if it's bad that it can be 0
+            if(internalPackets[dim][link].size() > 0){
                 aux = internalPackets[dim][link][0];
                 unsigned long long tsfint = aux->getCycleReceivedHeader();
                 aux = internalPackets[dim][link][internalPackets[dim][link].size()-1];
@@ -251,6 +252,7 @@ TrafficAnalysis::StatusAnalysis InternalAnalysis::createReportsForAllLinks() {
 
     //TODO: header
 
+    //TODO: verify other topologies (this was written for mesh)
     for(unsigned int dim = 0; dim < internalPackets.size(); dim++){
         for(unsigned int link = 0; link < internalPackets[dim].size(); link++){
             if(internalPackets[dim][link].size() > 0){
@@ -259,11 +261,10 @@ TrafficAnalysis::StatusAnalysis InternalAnalysis::createReportsForAllLinks() {
                     printf("ERROR: Impossible to create file %s",str);
                     return TrafficAnalysis::NoOutputFile;
                 } else {
-                    sprintf(str,"%.1f\t%.1f\t%.1f",
+                    sprintf(str,"%.1f\t%.5f\t%.5f",
                             linkAvCpF[dim][link],
                             linkAvgBandwidth[dim][link],
                             linkThroughput[dim][link]);
-                    //TODO: check where to use individual acceptedTraffic for individual pkts
                     // Replace ',' by '.'
                     unsigned int i = 0;
                     do {
@@ -271,12 +272,168 @@ TrafficAnalysis::StatusAnalysis InternalAnalysis::createReportsForAllLinks() {
                         i++;
                     }while(str[i] != '\0');
                     fprintf(fpOut,"%s\n",str);
-                    //TODO: add path of each packet
+
+                    //Accepted Traffic per packet - to identify flux path
+                    for(unsigned int p = 0; p < internalPackets[dim][link].size(); p++){
+                        PacketInfo *info = internalPackets[dim][link][p];
+                        sprintf(str,"%.5f\t%ull\t%i\t%i",
+                                acceptedTraffic[dim][link][p],
+                                info->getFlowId(),
+                                info->getSource(),
+                                info->getDestination());
+                        // Replace ',' by '.'
+                        i = 0;
+                        do {
+                            if(str[i] == ',') str[i] = '.';
+                            i++;
+                        }while(str[i] != '\0');
+                        fprintf(fpOut,"%s\n",str);
+                    }
+
                     fclose(fpOut);
                 }
             }
         }
     }
+
+    //Textual Maps
+    //obs: for mesh2d, dim 0-3 => E,W,N,S
+    std::string aux = "\nAverage Bandwidth\n";
+    unsigned int nodeId, linkId;
+    for(int y = topology->sp.getYSize() - 1; y >= 0; y--){
+        for(unsigned int x = 0; x < topology->sp.getXSize(); x++){
+            nodeId = COORDINATE_2D_TO_ID(x,y,topology->sp.getXSize());
+            linkId = COORDINATE_2D_TO_ID(x,y,topology->sp.getXSize()-1);
+            if(x < topology->sp.getXSize() - 1){
+                sprintf(str, "(%03d) %.2f  %.2f ", nodeId, linkAvgBandwidth[0][linkId], linkAvgBandwidth[1][linkId]);
+                //std::cout << "X=" << x << "; Y=" << y << "; linkAvgBw=" << linkAvgBandwidth[0][id] << std::endl;
+            }else{
+                sprintf(str, "(%03d)\n", nodeId);
+            }
+            aux.append(str);
+        }
+        if(y > 0){
+            for(unsigned int x = 0; x < topology->sp.getXSize(); x++){
+                linkId = COORDINATE_2D_TO_ID(x,y-1,topology->sp.getXSize());
+                if(x < topology->sp.getXSize() - 1){
+                    sprintf(str, "%.2f             ", linkAvgBandwidth[3][linkId]);
+                }else{
+                    sprintf(str, "%.2f\n\n", linkAvgBandwidth[3][linkId]);
+                }
+                aux.append(str);
+            }
+
+            for(unsigned int x = 0; x < topology->sp.getXSize(); x++){
+                linkId = COORDINATE_2D_TO_ID(x,y-1,topology->sp.getXSize());
+                if(x < topology->sp.getXSize() - 1){
+                    sprintf(str, "%.2f             ", linkAvgBandwidth[2][linkId]);
+                }else{
+                    sprintf(str, "%.2f\n", linkAvgBandwidth[2][linkId]);
+                }
+                aux.append(str);
+            }
+        }
+    }
+
+    sprintf(str,"%s/text_maps",resultDir);
+    if( (fpOut=fopen(str,"a+t")) == NULL ) {
+        printf("ERROR: Impossible to create file %s",str);
+        return TrafficAnalysis::NoOutputFile;
+    } else {
+        fprintf(fpOut,aux.c_str());
+    }
+    fclose(fpOut);
+
+    //todo: max/avg/min
+    aux = "\nAverage Cycles per Flit\n";
+    for(int y = topology->sp.getYSize() - 1; y >= 0; y--){
+        for(unsigned int x = 0; x < topology->sp.getXSize(); x++){
+            nodeId = COORDINATE_2D_TO_ID(x,y,topology->sp.getXSize());
+            linkId = COORDINATE_2D_TO_ID(x,y,topology->sp.getXSize()-1);
+            if(x < topology->sp.getXSize() - 1){
+                sprintf(str, "(%03d) %.2f  %.2f ", nodeId, linkAvCpF[0][linkId], linkAvCpF[1][linkId]);
+                //std::cout << "X=" << x << "; Y=" << y << "; linkAvgBw=" << linkAvgBandwidth[0][id] << std::endl;
+            }else{
+                sprintf(str, "(%03d)\n", nodeId);
+            }
+            aux.append(str);
+        }
+        if(y > 0){
+            for(unsigned int x = 0; x < topology->sp.getXSize(); x++){
+                linkId = COORDINATE_2D_TO_ID(x,y-1,topology->sp.getXSize());
+                if(x < topology->sp.getXSize() - 1){
+                    sprintf(str, "%.2f             ", linkAvCpF[3][linkId]);
+                }else{
+                    sprintf(str, "%.2f\n\n", linkAvCpF[3][linkId]);
+                }
+                aux.append(str);
+            }
+
+            for(unsigned int x = 0; x < topology->sp.getXSize(); x++){
+                linkId = COORDINATE_2D_TO_ID(x,y-1,topology->sp.getXSize());
+                if(x < topology->sp.getXSize() - 1){
+                    sprintf(str, "%.2f             ", linkAvCpF[2][linkId]);
+                }else{
+                    sprintf(str, "%.2f\n", linkAvCpF[2][linkId]);
+                }
+                aux.append(str);
+            }
+        }
+    }
+
+    sprintf(str,"%s/text_maps",resultDir);
+    if( (fpOut=fopen(str,"a+t")) == NULL ) {
+        printf("ERROR: Impossible to create file %s",str);
+        return TrafficAnalysis::NoOutputFile;
+    } else {
+        fprintf(fpOut,aux.c_str());
+    }
+    fclose(fpOut);
+
+    aux = "\nThroughput\n";
+    for(int y = topology->sp.getYSize() - 1; y >= 0; y--){
+        for(int x = 0; x < topology->sp.getXSize(); x++){
+            nodeId = COORDINATE_2D_TO_ID(x,y,topology->sp.getXSize());
+            linkId = COORDINATE_2D_TO_ID(x,y,topology->sp.getXSize()-1);
+            if(x < topology->sp.getXSize() - 1){
+                sprintf(str, "(%03d) %.2f  %.2f ", nodeId, linkThroughput[0][linkId], linkThroughput[1][linkId]);
+                //std::cout << "X=" << x << "; Y=" << y << "; linkAvgBw=" << linkAvgBandwidth[0][id] << std::endl;
+            }else{
+                sprintf(str, "(%03d)\n", nodeId);
+            }
+            aux.append(str);
+        }
+        if(y > 0){
+            for(int x = 0; x < topology->sp.getXSize(); x++){
+                linkId = COORDINATE_2D_TO_ID(x,y-1,topology->sp.getXSize());
+                if(x < topology->sp.getXSize() - 1){
+                    sprintf(str, "%.2f             ", linkThroughput[3][linkId]);
+                }else{
+                    sprintf(str, "%.2f\n\n", linkThroughput[3][linkId]);
+                }
+                aux.append(str);
+            }
+
+            for(int x = 0; x < topology->sp.getXSize(); x++){
+                linkId = COORDINATE_2D_TO_ID(x,y-1,topology->sp.getXSize());
+                if(x < topology->sp.getXSize() - 1){
+                    sprintf(str, "%.2f             ", linkThroughput[2][linkId]);
+                }else{
+                    sprintf(str, "%.2f\n", linkThroughput[2][linkId]);
+                }
+                aux.append(str);
+            }
+        }
+    }
+
+    sprintf(str,"%s/text_maps",resultDir);
+    if( (fpOut=fopen(str,"a+t")) == NULL ) {
+        printf("ERROR: Impossible to create file %s",str);
+        return TrafficAnalysis::NoOutputFile;
+    } else {
+        fprintf(fpOut,aux.c_str());
+    }
+    fclose(fpOut);
 
     return TrafficAnalysis::Ok;
 }
